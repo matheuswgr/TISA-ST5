286c997fa3c5beab0bbf678fcd6d6c774a4e9374
matheus.wagner <matheus.wagner@e-component.com>
2022-04-25 23:36:19 -0300

    working simulation and controller, but not cloud

---
+++ solar2022/m.sh
@@ -0,0 +1,5 @@
+gcc -o solar2022 solar2022.c planta.c relogio.c console.c instrumentacao.c evento.c -lpthread -lncurses -Wall
+gcc -o teste-controle teste-controle.c
+gcc -o teste-nuvem teste-nuvem.c
+
+
